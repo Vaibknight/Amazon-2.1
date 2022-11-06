962fc2ceefcf1d10879c8cec2b37778176a3e675
Vaibhav Tiwari <91458326+Vaibknight@users.noreply.github.com>
2022-11-06 21:41:39 +0530

    First Commit

---
+++ Amazon/app/static/js/firebase.js
@@ -0,0 +1,19 @@
+ 
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyD-eXYt_8tu9oSsRCEy3A1i8PISra2xmQs",
+    authDomain: "clone-2-f785c.firebaseapp.com",
+    projectId: "clone-2-f785c",
+    storageBucket: "clone-2-f785c.appspot.com",
+    messagingSenderId: "839029015011",
+    appId: "1:839029015011:web:8cb553e6a009becc68e0d6",
+    measurementId: "G-NQNRTP6V1L"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var db = firebase.firestore(); 
+
+
---
+++ Amazon/app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from.models import Orders
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+
+def cart(request):
+    if request.method == "POST":
+        items_json = request.POST.get('itemsJson','')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '')+" "+ request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+
+
+        order = Orders(items_json=items_json, name=name,email=email,phone=phone,address=address,city=city,state=state,zip_code=zip_code)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request,'cart.html',{'thank': thank , 'id': id})
+    return render(request,'cart.html')
